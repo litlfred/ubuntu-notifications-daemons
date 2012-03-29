bf8bf8a507aa6b4313743ac7940c998afb88546d
Carl Leitner <litlfred@ibiblio.org>
2012-03-29 16:11:37 -0400

    added oauth authentication and configuration file

---
+++ config.py
@@ -0,0 +1,31 @@
+#config.py for ubuntu twitter notification
+#copy this file to ~/.twitternotify
+#
+#first create an app under your account:
+#   https://dev.twitter.com/apps/new
+#next get the access token (key) and secret follow:
+#  https://dev.twitter.com/docs/auth/tokens-devtwittercom
+#
+#
+#put the access token (key) here
+a_key = "XXXXXXXXX-XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX"
+#
+#
+#put the access token secret here
+a_secret = "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX"
+#
+#
+#put the consumer key here
+c_key ="XXXXXXXXXXXXXXXXXXXX"
+#
+#
+#put the consumer secret gere
+c_secret = "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX"
+
+
+# the time to wait in seconds between twitter access
+wait=5 
+
+
+#set to 1 to show a backlog of messages on startup. 0 to suprress
+backlog=1
--- twitter-notify.py
+++ twitter-notify.py
@@ -1,18 +1,38 @@
 #!/usr/bin/env python
 
-usr="username" # username 
-pwd="password" # password
-wait=15 # in seconds
+
+import twitter
+import time
+import pynotify
+from urllib import urlretrieve
+import tempfile
+import os
+
+wait=15 #default is 15 seconds but can be overridden in config.py
+execfile(os.path.expanduser("~/.twitternotify/config.py"))
+
 
 ################################################################################
 ##
 ## Info: 
-##    Twitter notifications daemon
+##    Python Twitter notifications daemon for ubuntu notifyosd
 ## 
 ## Dependencies:
 ##    python-twitter
-##       http://code.google.com/p/python-twitter/
-##       (apt-get install python-twitter)
+##       source is found here:  http://code.google.com/p/python-twitter/
+##       the currently available package for ubuntu (0.6) does not support oAuth. see the bug:
+##          https://bugs.launchpad.net/ubuntu/+source/python-twitter/+bug/746850
+##       and indicate that it affects you.  in the meantime you can do:
+##           hg clone http://python-twitter.googlecode.com/hg/ python-twitter
+##	     cd python-twitter
+##	     hg update
+##           python setup.py build
+##	     sudo python setup.py install
+##       once the above bug is resolved you will be able to do instead:
+##           apt-get install python-twitter)
+##
+##Configure:
+##    see config.py for instructions
 ##
 ## Run:
 ##    python twitter-notify.py 
@@ -22,11 +42,15 @@ wait=15 # in seconds
 ##
 ##			actual daemon will happen soon enough
 ##
-## Copyright 2009 James Wilson
+## Copyright 2009 James Wilson, 2012 Carl Leitner
 ##
 ## Author:
 ##    James Wilson <j@meswilson.com>
 ##    http://ja.meswilson.com/blog/
+##
+## 
+##    Carl Leitner <litlfred@ibiblio.org>
+##    https://github.com/litlfred
 ##
 ## This program is free software: you can redistribute it and/or modify it
 ## under the terms of the GNU General Public License version 3, as published
@@ -42,43 +66,56 @@ wait=15 # in seconds
 ##
 ################################################################################
 
-import twitter
-import time
-import pynotify
+
 
 if not pynotify.init ("icon-summary-body"):
 	sys.exit(1)
 
 api = 0
 def login():
-	global api, usr, pwd
+	global api,c_key,c_secret,a_key,a_secret
 	try:
-		api = twitter.Api(username=usr, password=pwd)
+		api = twitter.Api(consumer_key=c_key, consumer_secret=c_secret, access_token_key=a_key, access_token_secret=a_secret)
 	except:
 		time.sleep(15)
 		return login()
-
 login()
 
 all = []
+icons = {}
 
-def notify(usr,message,icon="notification-message-IM"):
-	n = pynotify.Notification (usr,message,icon)
+def geticon(url):
+	global icons
+	if url in icons.viewkeys():
+		file = icons[url]
+	else:
+		f = tempfile.NamedTemporaryFile()
+		f.close()
+		file = f.name
+		urlretrieve(url,file)
+		icons[url] = file
+	return file
+
+
+def notify(user,message):
+	n = pynotify.Notification (user.screen_name,message,geticon(user.profile_image_url))
 	n.show()
+
 while 1:
 	try:	
-		stat = api.GetFriendsTimeline()
+		stat = api.GetFriendsTimeline(count=5)
+		
 	except:
-		time.sleep(15)
+		time.sleep(wait)
 		login()
 	else:
-		if len(all) == 0:
+		if len(all) == 0 and backlog == 0:
 			for s in stat:
 				all.append(s.text)
 		for s in stat:
 			if not s.text in all:
-				notify(s.user.screen_name, s.text)
+				notify(s.user,s.text)
 				all.append(s.text)
 
 		time.sleep(wait)
-#	print [s.text for s in stat]
+
